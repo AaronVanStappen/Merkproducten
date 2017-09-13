0eba04dd739509ce1ebe97514cf3232553b56571
Aaron <aaronvanstappen@gmail.com>
2017-09-13 14:19:29 +0200

    Klasse Deodorant afgewerkt.

--- src/be/oak3/model/Deodorant.java
+++ src/be/oak3/model/Deodorant.java
@@ -1,4 +1,11 @@
 package be.oak3.model;
 
-public class Deodorant {
+public class Deodorant extends Product {
+    public enum DeoType {VAPO, STICK};
+    private DeoType soort;
+
+    public Deodorant(int productNr, String merk, String naam, int volume, double prijs, DeoType soort) {
+        super(productNr, merk, naam, volume, prijs);
+        this.soort = soort;
+    }
 }
